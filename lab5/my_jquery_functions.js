07e2ce19492dc0de290196f25eff780d8b58edd6
MunaShabab <80338619+MunaShabab@users.noreply.github.com>
2023-01-25 13:28:34 -0500

    Add files via upload

---
+++ lab5/my_jquery_functions.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+ $( function() {
+    $( "#accordion" )
+      .accordion({
+		collapsible:true,
+		heightStyle:"content",
+		header: ">div>h3"
+      })
+	  .sortable({
+        axis: "y",
+        handle: "h3",
+        stop: function( event, ui ) {
+          // IE doesn't register the blur when sorting
+          // so trigger focusout handlers to remove .ui-state-focus
+          ui.item.children( "h3" ).triggerHandler( "focusout" );
+ 
+          // Refresh accordion to handle new order
+          $( this ).accordion( "refresh" );
+        }
+      });
+     
+     
+  });
+
+});
+
+
+
